ec932ffdcaf2e7783bb19d4b4d52254b88db06bd
Shaikh-Mustafa <mrmustafashaikh111@gmail.com>
2022-04-29 23:36:43 +0530

    Linked Css and Js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 /*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
